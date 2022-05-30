7e04078dd71f41c4667818bfe19686a9df5e9bcd
damduong <duong.dam@classfunc.com>
2022-05-30 12:31:48 +0700

    Updates

---
+++ precache-manifest.053ac3157aeab22f18570d7d50382f23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa4028afefffd3f71786d6b9432893c9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "174118ee2d780d09da85",
+    "url": "./static/css/main.257f4988.chunk.css"
+  },
+  {
+    "revision": "0eb4a7865fbbc64e6cd8",
+    "url": "./static/js/2.9bf3350e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.9bf3350e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "174118ee2d780d09da85",
+    "url": "./static/js/main.0f66e3cf.chunk.js"
+  },
+  {
+    "revision": "971c200393d0e57cccbb",
+    "url": "./static/js/runtime-main.75351ce4.js"
+  }
+]);
